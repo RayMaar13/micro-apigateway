431467626913a37237d8bfac3a3b1ba8ad397c49
SFGE23 <ammarmart1@gmail.com>
2024-05-02 17:33:09 +0700

    couple change and fix bug

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require("cors");
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -28,6 +29,7 @@ app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({extended: false, limit: "50mb"}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
